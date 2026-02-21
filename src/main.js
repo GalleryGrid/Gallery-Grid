e82d1f559c633130d756ec2e3a39c7c7f91dcfcf
Bryan <bryan001700@gmail.com>
2026-02-21 13:16:20 -0500

    completed the event listeners and rending a single art piece once more info is clicked

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { renderArt, renderArtDetails } from './dom-helpers.js';
 import { fetchData, getArt, getArtById } from './fetch-helpers.js';
+import './event-listener.js';
 
-let currentPage = 1;
 const errorMessage = document.querySelector('#error-message');
-getArt(currentPage).then((res) => {
+getArt(1).then((res) => {
   if (res.error) {
     errorMessage.textContent = res.error.message;
   } else {
